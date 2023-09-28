7c8a06bc1675e750454fd1d3889ae1a7a6e2eddf
Cash Myers <cmyers880@gmail.com>
2023-09-27 22:39:54 -0500

    Initial Commit

--- client/src/components/firebaseConfig/firebaseConfig.js
+++ client/src/components/firebaseConfig/firebaseConfig.js
@@ -6,13 +6,22 @@ import { getFirestore } from 'firebase/firestore'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
-const firebaseConfig = {
-    apiKey:process.env.REACT_APP_apiKey,
-    authDomain:process.env.REACT_APP_authDomain ,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId:process.env.REACT_APP_appId
+// const firebaseConfig = {
+//     apiKey:process.env.REACT_APP_apiKey,
+//     authDomain:process.env.REACT_APP_authDomain ,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId:process.env.REACT_APP_appId
+//   };
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0Y4Pf5HBS0ZCiTEpKI6RlB22OslxL51U",
+    authDomain:"chatapp-d9dd0.firebaseapp.com",
+    projectId: "chatapp-d9dd0",
+    storageBucket: "chatapp-d9dd0.appspot.com",
+    messagingSenderId: "193428859362",
+    appId:"1:193428859362:web:628aa98e30ab48e62831bb"
   };
 
 // Initialize Firebase
--- server/app.js
+++ server/app.js
@@ -1,50 +1,42 @@
-const express = require('express')
-const http  = require('http')
-const Server  = require("socket.io").Server
-const app = express()
-const path  = require('path')
+const express = require("express");
+const http = require("http");
+const Server = require("socket.io").Server;
+const app = express();
+const path = require("path");
 
-const server  = http.createServer(app)
-const io = new Server(server , {
-    cors:{
-        origin:"*"
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
     }
-})
+  );
+});
 
+io.on("connection", (socket) => {
+  console.log(`Socket ${socket.id} connected`);
 
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "../client/build");
+  socket.on("chat", (chat) => {
+    io.emit("chat", chat);
+  });
 
-app.use(express.static(buildPath))
+  socket.on("disconnect", () => {
+    console.log(`=> Socket ${socket.id} disconnected`);
+  });
+});
 
-app.get("/*", function(req, res){
-
-    res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
-        function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-
-})
-
-
-
-
-io.on("connection" , (socket) => {
-   console.log('We are connected')
-
-   socket.on("chat" , chat => {
-      io.emit('chat' , chat)
-   } )
-
-   socket.on('disconnect' , ()=> {
-    console.log('disconnected')
-   })
-})
-
-
-
-server.listen(5001 , () => console.log('Listening to port 5001'))
+server.listen(5001, () => console.log("Listening to port 5001"));
